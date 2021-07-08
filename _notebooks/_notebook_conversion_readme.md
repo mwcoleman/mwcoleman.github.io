bdc90213ef067407ab949996566f1fcafe2b0534
Matt <mwc11235@gmail.com>
2021-07-08 18:19:53 +1000

    modified md conversion script. Cleaned up

---
+++ _notebooks/_notebook_conversion_readme.md
@@ -0,0 +1,5 @@
+New notebook files go in their own folder within _notebooks
+
+when ready to convert to md, nav to _notebooks and "bash convert.sh folder/file" without .ipynb extension
+
+This will nbconvert the ipynb to md, strip svg reference and replace with html for png, add yaml header, copy image files to _images/ an
--- _notebooks/edit.py
+++ _notebooks/edit.py
@@ -7,7 +7,9 @@ import sys, re, datetime, os
 def edit():
     today_str = datetime.date.today().strftime("%Y-%m-%d")
     old_filename = str(sys.argv[1])
-    new_filename = today_str+"-"+old_filename
+    new_filename = re.search(r"/(\w+)",str(sys.argv[1])).group(1)+".md"
+    print(new_filename)
+    new_filename = today_str+"-"+new_filename
     
     path = "/home/matt/Documents/github_page/mwcoleman.github.io/" 
     yaml_args = sys.argv
@@ -18,10 +20,10 @@ def edit():
     filedata = re.sub(r"!\[svg\]\(", "<img src=\"../images/", filedata)
     filedata = re.sub(".svg\)", ".png\">", filedata)
     filedata = yaml + filedata
-    with open(path+"_posts/"+old_filename, 'w') as file:
+    with open(path+"_posts/"+new_filename, 'w') as file:
         file.write(filedata)
     # Name it correctly for jekyll
-    os.rename(path+"_posts/"+old_filename, path+"_posts/"+new_filename) 
-
+    # os.rename(path+"_posts/"+old_filename, path+"_posts/"+new_filename) 
+    
 if __name__ == '__main__':
     edit()
